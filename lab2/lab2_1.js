44e3048e63c5842a8860a5ab95f7bb78bdb075d6
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2025-02-11 09:34:27 +0200

    add_lab2

---
+++ lab2/lab2_1.js
@@ -0,0 +1,9 @@
+/* Доповни код, надавши змінному totalPrice виразу для підрахунку загальної суми замовлення.
+Змінна pricePerItem зберігає ціну однієї одиниці товару,
+а orderedQuantity - кількість одиниць товару на замовлення. */
+
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+
+// Change code below this line
+const totalPrice = pricePerItem;
---
+++ lab2/lab2_3.js
@@ -0,0 +1,9 @@
+/* Оголоси змінну message та запиши в неї повідомлення про покупку,
+рядок у форматі: "You picked <имя товара>, price per item is <цена товара> credits".
+ Де <имя товара> це <цена товара> значення змінних productName і pricePerItem.
+  Використовуй синтаксис шаблонних рядків. */
+
+const productName = "Droid";
+const pricePerItem = 3500;
+
+// Change code below this line
